04efc56a57861ab5253892f2d157f95e39401d36
Giovanni-LN <giovanni.lima.nascimento@gmail.com>
2023-12-15 15:11:04 -0300

    Questão-02/Reformulação

--- Questão-02/main.asm
+++ Questão-02/main.asm
@@ -1,8 +1,8 @@
 .data
 buffer: .space 1        # Espaço para armazenar o caractere lido
 
-.text
-.globl main
+    .text
+    .globl main
 
 main:
     li $t0, 0xffff0000  # Endereço do registrador Receiver Control (KEYBOARD)
@@ -17,15 +17,15 @@ connect_to_mips:
 
 main_loop:
     # Leitura do teclado (KEYBOARD)
-    lb $t4, 0($t0)      # Lê o registrador Receiver Control
-    andi $t4, $t4, 0x1  # Isola o bit de pronto
+    lw $t4, 0($t0)      # Leitura do registrador Receiver Control
+    andi $t4, $t4, 0x1  # Máscara para obter o bit de pronto
     beqz $t4, main_loop # Se o bit de pronto for 0, continua no loop
 
-    lb $t5, 0($t1)      # Lê o registrador Receiver Data
+    lw $t5, 0($t1)      # Leitura do registrador Receiver Data
 
     # Impressão no display (DISPLAY)
-    sb $t5, 0($t3)      # Escreve o caractere no registrador Transmitter Data
+    sw $t5, 0($t3)      # Escreve o caractere no registrador Transmitter Data
     li $t6, 0x1         # Prepara o bit de pronto no registrador Transmitter Control
-    sb $t6, 0($t2)      # Ativa o bit de pronto no registrador Transmitter Control
+    sw $t6, 0($t2)      # Ativa o bit de pronto no registrador Transmitter Control
 
     j main_loop         # Volta ao início do loop
